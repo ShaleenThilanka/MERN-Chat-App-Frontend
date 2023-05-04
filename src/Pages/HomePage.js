7e8c4ecb07726684dccbc9da46368954752d034f
ShaleenThilanka <shaleen.diss@gmail.com>
2023-05-04 12:52:59 +0530

    done login and singup ui

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import {Box, Container, Text} from '@chakra-ui/react'
+import {Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text} from '@chakra-ui/react'
+import Login from "../Components/Authentication/Login";
+import SingUp from "../Components/Authentication/SingUp";
 
 const HomePage = () => {
     return (
         <Container maxW="xl" centerContent>
           <Box
-          d="flex"
-          justifyContent={"center"}
-          p={3}
-          bg={"white"}
-          w={"100%"}
-          m={"40px 0 15px 0"}
-          borderRadius={"lg"}
-          borderWidth={"1px"}
+              d="flex"
+              justifyContent={"center"}
+              p={3}
+              bg={"white"}
+              w={"100%"}
+              m={"40px 0 15px 0"}
+              borderRadius={"lg"}
+              borderWidth={"1px"}
 
           >
-              <Text fontSize={"4xl"} fontFamily={"Work sans"} color={"black"}>Simple Chat</Text>
+              <Text fontSize={"4xl"} fontFamily={"Work sans"} color={"black"} textAlign={"center"}>Simple Chat</Text>
           </Box>
-            <Box>
-                <Text>Simple Chat</Text>
+            <Box   bg={"white"} w={"100%"}  p={4} borderRadius={"lg"} borderWidth={"1px"} color={"black"} >
+                <Tabs variant='soft-rounded' >
+                    <TabList mb={"1em"}>
+                        <Tab width={"50%"}>Login</Tab>
+                        <Tab width={"50%"}>SingUp</Tab>
+                    </TabList>
+                    <TabPanels>
+                        <TabPanel>
+                          <Login/>
+                        </TabPanel>
+                        <TabPanel>
+                          <SingUp/>
+                        </TabPanel>
+                    </TabPanels>
+                </Tabs>
             </Box>
         </Container>
     );
